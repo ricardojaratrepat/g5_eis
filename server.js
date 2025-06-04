c7dba8d0cfbf1307faafd93a37177d3dc7f6228a
grez-lucas <lucasgrez3010@gmail.com>
2025-06-04 18:04:24 -0400

    feat(server): Add socket.io message handlers

--- server.js
+++ server.js
@@ -14,6 +14,53 @@ app.use(express.static('public'));
 let connectedUsers = 0;
 let drawingHistory = [];
 
+// Message Handlers: Each one of these handles a WebSocket Message from the client
+const messageHandlers = {
+  NEW_DRAWING: (socket, data) => {
+    drawingHistory.push(data);
+    // Broadcast the message to all connected clients (except the sender)
+    socket.broadcast.emit('draw', data);
+  },
+  ERASE_DRAWING: (socket, data) => {
+    // TODO: Identify and remove specific drawing objects
+    // for now it just clears a rectangular area
+    drawingHistory.push(data);
+    socket.boradcast.emit('erase', data);
+  },
+  REQUEST_HISTORY: (socket) => {
+    socket.emit('drawingHistory', drawingHistory)
+  }
+  // More messages go here as the app grows
+
+};
+
+io.on('connection', (socket) => {
+  connectedUsers++;
+  io.emit('userCount', connectedUsers);
+  console.log('New client connected. Users:', connectedUsers);
+
+  // Send drawing history to new client
+  messageHandlers.REQUEST_HISTORY(socket);
+
+  // This forwards messages to our handler
+  socket.on('message', (message) => {
+    const { type, payload } = message;
+    const handler = messageHandlers[type];
+    if (handler) {
+      handler(socket, payload);
+    } else {
+      console.log(`No handler for message type: ${type}`);
+    }
+  });
+
+  // This handles disconnects
+  socket.on('disonnect', () => {
+    connectedUsers--;
+    io.emit('userCount', connectedUsers);
+    console.log('Client disconnected. Users:', connectedUsers);
+  });
+
+})
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
